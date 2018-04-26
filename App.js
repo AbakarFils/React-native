21f18b88b2b25b52050b3dfa467212aa61c79a59
Abakar-fils <31525181+Abakar-fils@users.noreply.github.com>
2018-04-26 05:12:47 +0200

    mise en forme du fichier App.js ( Coté TabNavigation)

--- App.js
+++ App.js
@@ -3,13 +3,34 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import About from './composant/About'
 import Search from './composant/Search'
 import { TabNavigator } from "react-navigation"
-
+//***************************
+// pour avoir le Menu de Navigation en bas
+// */
 
 const Tabs = TabNavigator(
   {
     Search: { screen: Search },
     About: { screen: About }
+  },
+  {
+    tabBarPosition: 'bottom',
+    tabBarOptions : {
+      showIcon: true,
+      showLabel: false,
+      pressColor: 'white',
+      indicatorStyle: {
+        height :2,
+        backgroundColor: "#FFF"
+      },
+      style: {
+        backgroundColor: "#a2273C" ,// couleur du tabnavigateur
+        borderTopWidth: 1,
+        borderColor: "#3f101C",
+
+      }
+    }
   }
+  
 )
 export default class App extends React.Component {
   render() {
